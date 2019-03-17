007e0da5ba1c56572fbb22fe24a87960b3de871f
Shehan <shehan.salinda92@gmail.com>
2019-03-18 01:06:40 +0530

    Change auth service

--- app/src/main/java/unme/app/com/ume/LoginActivity.java
+++ app/src/main/java/unme/app/com/ume/LoginActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
